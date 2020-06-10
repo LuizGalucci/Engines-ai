24ca14bd441b0d4b8c2073b869ba319e64daafb4
Bruno <buhlovatotomlinson@gmail.com>
2020-06-09 21:23:15 -0300

    prefab do inimigo e do player
    animações aplicadas em ambos

---
+++ Assets/Scripts/IA.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IA : MonoBehaviour
+{
+    public enum EstadoIA {
+        Atacando,
+        Andando,
+    }
+
+    public EstadoIA estado;
+    public float dano;
+    public Animator controladorAnimacao;
+
+    NavMeshAgent agenteNM;
+    Vida vida;
+    Vida vidaJogador;
+
+    void Awake() {
+        agenteNM = GetComponent<NavMeshAgent>();
+        vida = GetComponent<Vida>();
+        vidaJogador = GameObject.FindWithTag("Player").GetComponent<Vida>();
+    }
+
+    void Update() {
+        if (agenteNM.isStopped) {
+            estado = EstadoIA.Atacando;
+        } else {
+            estado = EstadoIA.Andando;
+        }
+
+        controladorAnimacao.SetFloat("velocidade", agenteNM.velocity.magnitude);
+
+        if (vida.vida <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Atacar() {
+        vidaJogador.vida = vidaJogador.vida - dano;
+    }
+}
