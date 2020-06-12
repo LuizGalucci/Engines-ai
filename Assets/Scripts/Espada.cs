58a3cef14da81ea27897575967ff5d33986873b3
Bruno <buhlovatotomlinson@gmail.com>
2020-06-11 22:52:31 -0300

    Sons

---
+++ Assets/Scripts/Espada.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Espada : MonoBehaviour
+{
+    public float dano;
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.CompareTag("Inimigo")) {
+            Vida vidaInimigo = col.gameObject.GetComponent<Vida>();
+            vidaInimigo.DiminuirVida(dano);
+        }
+    }
+}
